5bccbb880e2d2b71db80515a4ce4ff35cd38bd2a
MARCGRAFIKS <marcgrafiks@gmail.com>
2026-05-06 14:30:39 +0000

    Ici, utilisation des classes group et board et ajout de quelque fonction

--- item.cc
+++ item.cc
@@ -105,13 +105,79 @@ void Animation::next() {
 ///////////////////////////////////classe groupe ///////////////////////////////:
 
 void Group::draw() {
-   for(auto* item : items) {
-        item->draw();
-    }
+     std::sort(items.begin(), items.end(), [](Item*a, Item* b){
+      return a->z <b->z;
+     });
+
+     for(auto* item : items) {
+      item->draw();
+     }     
+}
+
+void Group::draw(double angle) {
+    
+     for(auto& item : gemItems) {
+      item->draw(angle);
+     }
+        
 }
     
 void Group::addRefe(Item* other) {
+    if(std::find(items.begin(), items.end(), other) == items.end())
     items.push_back(other);
+}
+
+void Group::remove(Item* other) {
+    items.erase(std::remove(items.begin(), items.end(), other), items.end());
+}
+    
+void Group::update(int tick) {
+    for(auto* item : items) {
+        item->update(tick);
+    }
+}
+
+void Group::add(std::unique_ptr<Item> item) {
+    gemItems.push_back(std::move(item));
+}
+
+void Group::spawnItems(int count, SDL_Renderer* rend) {
+    for (int i = 0; i < count; i++) {
+        auto gem = std::make_unique<Item>();
+        gem->setRenderer(rend);
+        gem->loadFromImage("terre.png");
+        gem->setSize(32, 32);
+        gem->setPos(rand() % 750, rand() % 450);
+
+        add(std::move(gem));
+    }
+}
+    
+// fonctionspécialisées
+void Group::handleClick(int x, int y) {
+    for(auto* item : items) {
+        std::cout << "tester si clic dans pos"<< std::endl;
+    }
+}
+    
+void Group::checkCollision(int x, int y) {
+    for(auto* a : items) {
+        for(auto* b : items) {
+            if(a!=b) {
+                std::cout << " test collision \n" << std::endl;
+            }
+        }
+    }
+}
+    
+// ajout du filtres 
+std::vector<Item*> Group::getVisible() {
+    std::vector<Item*> result;
+    for(auto* item : items) {
+        // if(item->isVisible())
+        result.push_back(item);
+    }
+    return result;
 }
     
 void Group::move(int x, int y) {
@@ -126,19 +192,63 @@ Board::Board(SDL_Renderer* rend) {
     render = rend;
     int w, h;
     SDL_GetRendererOutputSize(render, &w, &h);
+    player.setRenderer(render);
+    player.loadAnimation("person/JK_P_Sword__Run_", "000", ".png");
     player.setPos(w/2-32, h/2-32);
+    player.setSize(64, 64);
+
+    gem.spawnItems(10, render);
+    
+    bkgr.setRenderer(render);
+    bkgr.loadFromImage("terre.png");
     bkgr.setSize(w, h);
     bkgr.setPos(0, 0);
+
+    drawn.addRefe(&player);
 }
 
 void Board::move(int x, int y) {
-    drawn.move(-x, -y);
+    drawn.move(x, y);
     click.move(-x, -y);
     collide.move(-x, -y);
 }
+
+void Board::update(int tick) {
+    // deplacement joueur
+    move(speedX, speedY);
+    // update des groupes
+    drawn.update(tick);
+    click.update(tick);
+    collide.update(tick);
+}
+
+void Board::handleEvent(const SDL_Event& ev) {
+    switch(ev.type) {
+        case SDL_KEYDOWN:
+                switch(ev.key.keysym.sym) {
+                    case SDLK_UP: speedY = -1; break;
+                    case SDLK_DOWN: speedY = 1; break;
+                    case SDLK_LEFT: speedX =-1; break;
+                    case SDLK_RIGHT: speedX = 1; break;
+                }
+                break;
+                case SDL_KEYUP:
+                switch(ev.key.keysym.sym) {
+                    case SDLK_UP:
+                    case SDLK_DOWN: speedY = 0; break;
+                    case SDLK_LEFT:
+                    case SDLK_RIGHT: speedX = 0; break;
+                }
+                break;
+    }
+}
     
 void Board::draw() {
+    double a = 1.1;
     bkgr.draw();
     drawn.draw();
-    player.draw();
+    gem.draw(a);
+    collide.draw();
+    click.draw();
+    a ++;
 }
--- item.hpp
+++ item.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include "renderer.hpp"
 #include <vector>
+#include <memory>
+#include <algorithm>
 
 void init_Item();
 void quit_Item();
@@ -17,6 +19,7 @@ class Item : public Renderer{
     void draw();
     void draw(double angle);
     virtual void update(int tick);
+    int z = 0;
 
     protected:
     SDL_Rect pos;
@@ -46,11 +49,22 @@ class Animation : public Item {
 class Group {
     public:
     void draw();
+    void draw(double angle);
     void addRefe(Item* other);
+    void remove(Item* other);
+    void update(int itck);
+    void add(std::unique_ptr<Item> item);
+    void spawnItems(int count, SDL_Renderer* rend);
+    // fonctionspécialisées
+    void handleClick(int x, int y);
+    void checkCollision(int x, int y);
+    // ajout du filtres 
+    std::vector<Item*> getVisible();
     void move(int x, int y);
 
     public:
     std::vector<Item*> items;
+    std::vector<std::unique_ptr<Item>> gemItems;
 };
 
 // ajout classe de board
@@ -58,13 +72,18 @@ class Board {
     public:
     Board(SDL_Renderer* rend);
     void move(int x, int y);
+    void update(int tick);
+    void handleEvent(const SDL_Event& ev);
     void draw();
 
     public:
     SDL_Renderer* render = nullptr;
     Group drawn; 
+    Group gem;
     Group click;  
     Group collide;
-    Item player;
+    Animation player;
     Item bkgr;
+    int speedX = 0;
+    int speedY = 0;
 };
